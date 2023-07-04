e3d46979ff85374d5eb1e9cb060c84537746e206
Brian Chaia <brian@banhaia.com>
2023-07-04 12:38:10 -0300

    feat: pagina basica con script clientify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Link from 'next/link';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div style={{ padding: '100px' }}>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-around',
+          alignItems: 'flex-start',
+          maxWidth: '400px',
+        }}
+      >
+        <Link href={'/'}>home</Link>
+        <Link href={'/page1'}>page 1</Link>
+        <Link href={'/page2'}>page 2</Link>
+      </div>
+      <Component {...pageProps} />
+    </div>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,47 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head></Head>
       <body>
+        {/* <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            var iwapp = document.createElement("whatsApp-root");
+            document.getElementsByTagName("body")[0].appendChild(iwapp);
+            window.Clientify_WhatsappBID = "124596";
+            window.Clientify_WhatsappUID = "31764";
+          `,
+          }}
+        /> */}
+
         <Main />
         <NextScript />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.Clientify_WhatsappBID = "124596";
+              window.Clientify_WhatsappUID = "31764";
+
+              document.addEventListener("DOMContentLoaded", function() {
+                var iwapp = document.createElement("whatsApp-root");
+                document.getElementsByTagName("body")[0].appendChild(iwapp);
+              
+                var script = document.createElement("script");
+                script.src = "https://api.clientify.net/static/js/whatsapp-widget/embed-whatsapp/embed.js";
+              
+                iwapp.appendChild(script);
+              });`,
+          }}
+        />
+        {/* <script
+          defer
+          type="text/javascript"
+          src="https://api.clientify.net/static/js/whatsapp-widget/embed-whatsapp/embed.js"
+        ></script> */}
       </body>
     </Html>
-  )
+  );
 }
